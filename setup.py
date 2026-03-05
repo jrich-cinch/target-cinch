12c20117c4d9fd1e9d9e0f815bde6ba8fd5110f9
Jared Rich <jrich@cinch.io>
2026-03-05 13:21:12 -0700

    inc vnum

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="target-cinch",
-    version="0.0.22",
+    version="0.0.23",
     description="Singer.io target for loading data into Cinch",
     author="Cinch",
     url="https://github.com/cinchio/target-cinch",
